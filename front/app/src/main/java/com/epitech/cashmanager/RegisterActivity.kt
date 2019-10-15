9fea7e4dbaec598d8ec73a7060d6a7b732f48b65
Nicolas KLEIN <nicolas6720@gmail.com>
2019-10-15 10:23:20 +0200

    add models and activity register

---
+++ front/app/src/main/java/com/epitech/cashmanager/RegisterActivity.kt
@@ -0,0 +1,22 @@
+package com.epitech.cashmanager
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+        setSupportActionBar(toolbar)
+
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+}
