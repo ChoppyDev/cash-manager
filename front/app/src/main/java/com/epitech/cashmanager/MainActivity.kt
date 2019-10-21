8c227b2287d3f640d203fb3292cd13d0b3c39521
romain <romain.floupin@epitech.eu>
2019-10-21 11:52:17 +0200

    dd

--- front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
+++ front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
@@ -1,12 +1,34 @@
 package com.epitech.cashmanager
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.epitech.stdlib.Singletons.Logger
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.button2)
+        val loginButton: Button = findViewById(R.id.connexionButton)
+        val i = Intent(this, RegisterActivity::class.java)
+        val loginIntent = Intent(this, CartActivity::class.java)
+
+        button.setOnClickListener {
+            startActivity(i)
+        }
+
+        loginButton.setOnClickListener {
+            startActivity(loginIntent)
+            this.finish() //finish login activity for security issues
+            // todo : Make a it in a better way
+        }
+        println("iso test ${Locale.getDefault()}")
+        Logger.noCommit("TESTSTSTSTTSTSTS")
     }
+
+
 }
---
+++ front/app/src/main/java/com/epitech/cashmanager/models/User.kt
@@ -0,0 +1,8 @@
+package com.epitech.cashmanager.models
+
+data class User(
+    val username: String,
+    val password: String,
+    var firstname: String,
+    var lastname: String
+)
