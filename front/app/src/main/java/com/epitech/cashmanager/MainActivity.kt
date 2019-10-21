75bb6ead35d7b40dde1930f78cd6a02312a735aa
romain <romain.floupin@epitech.eu>
2019-10-21 11:46:02 +0200

    Revert "revert"
    This reverts commit 68c4dff9312fa5c864d194a9b83beb29cda97015.

--- front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
+++ front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
@@ -1,43 +1,12 @@
 package com.epitech.cashmanager
 
-<<<<<<< HEAD
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-=======
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
-import android.widget.Button
-import com.epitech.stdlib.Singletons.Logger
-import java.util.*
->>>>>>> parent of 85b35e8... new version with navigation
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-<<<<<<< HEAD
     }
-=======
-        val button: Button = findViewById(R.id.button2)
-        val loginButton: Button = findViewById(R.id.connexionButton)
-        val i = Intent(this, RegisterActivity::class.java)
-        val loginIntent = Intent(this, CartActivity::class.java)
-
-        button.setOnClickListener {
-            startActivity(i)
-        }
-
-        loginButton.setOnClickListener {
-            startActivity(loginIntent)
-            this.finish() //finish login activity for security issues
-            // todo : Make a it in a better way
-        }
-        println("iso test ${Locale.getDefault()}")
-        Logger.noCommit("TESTSTSTSTTSTSTS")
-    }
-
-
->>>>>>> parent of 85b35e8... new version with navigation
 }
--- front/app/src/main/java/com/epitech/cashmanager/models/User.kt
+++
@@ -1,8 +0,0 @@
-package com.epitech.cashmanager.models
-
-data class User(
-    val username: String,
-    val password: String,
-    var firstname: String,
-    var lastname: String
-)
