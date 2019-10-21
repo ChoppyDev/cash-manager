ed59c5d14c2e07ac30375708514c3941f13b572c
Nicolas KLEIN <nicolas6720@gmail.com>
2019-10-21 16:19:09 +0200

    added .gitignore rule + test for data bindings

--- front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
+++ front/app/src/main/java/com/epitech/cashmanager/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.epitech.cashmanager
 
 import android.content.Intent
+import android.databinding.DataBindingUtil
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.epitech.cashmanager.databinding.ActivityMainBinding
+import com.epitech.cashmanager.models.User
 import com.epitech.stdlib.Singletons.Logger
 import java.util.*
 
@@ -11,7 +14,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding : ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.test  = User("kjhds", "test", "test", "test")
+        //setContentView(R.layout.activity_main)
         val button: Button = findViewById(R.id.button2)
         val loginButton: Button = findViewById(R.id.connexionButton)
         val i = Intent(this, RegisterActivity::class.java)
